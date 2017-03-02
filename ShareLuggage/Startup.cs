3f16289a27245488de5a0d829e11f2496f29bb51
pangxiong2008 <wuxiaonong@gmail.com>
2017-03-01 22:26:20 -0700

    Add project files.

---
+++ ShareLuggage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShareLuggage.Startup))]
+namespace ShareLuggage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
